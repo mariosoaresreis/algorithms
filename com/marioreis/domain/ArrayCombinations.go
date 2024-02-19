ae4db75f95c16dc3a6408407c21fd2161c50aeb6
mariosoaresreis <mario_reis_jr@hotmail.com>
2024-02-19 18:46:43 -0300

    store results in a slice

--- com/marioreis/domain/ArrayCombinations.go
+++ com/marioreis/domain/ArrayCombinations.go
@@ -3,33 +3,32 @@ package domain
 import "fmt"
 
 func ArrayCombinations(slice []int) {
-	combine(0, slice, []int{})
+	finalResult := make([][]int, 0)
+	combine(0, slice, []int{}, &finalResult)
+	fmt.Println(finalResult)
 }
 
-func combine(start int, originalSlice []int, subSlice []int) [][]int {
+func combine(start int, originalSlice []int, subSlice []int, finalResult *[][]int) {
 	maxSize := len(originalSlice)
 
 	if start > maxSize-1 {
-		return nil
+		return
 	}
 
 	if maxSize <= len(subSlice) {
-		return nil
+		return
 	}
-
-	finalResult := make([][]int, 0)
 
 	for i := start; i < maxSize; i++ {
 		tempSlice := make([]int, len(subSlice))
 		copy(tempSlice, subSlice)
 		tempSlice = append(tempSlice, originalSlice[i])
-		finalResult = append(finalResult, tempSlice)
-		fmt.Println(tempSlice)
+		*finalResult = append(*finalResult, tempSlice)
+		//fmt.Println(tempSlice)
 
 		if i+1 < maxSize {
-			combine(i+1, originalSlice, tempSlice)
+			combine(i+1, originalSlice, tempSlice, finalResult)
 		}
 	}
 
-	return finalResult
 }
