ff154f4129b9b090385d6fb9222f8f4c35ce8cd3
Mario Reis <mario_reis_jr@hotmail.com>
2024-06-15 18:15:50 -0300

    TDD: word wrap

---
+++ com/marioreis/tdd/WordWrap_test.go
@@ -0,0 +1,56 @@
+package tdd
+
+import (
+	"strings"
+	"testing"
+)
+
+func Test_Wrap(t *testing.T) {
+	if !assertWrap("hello", 5, "hello") {
+		t.Error("failed!")
+	}
+}
+
+func Test_Wrap2(t *testing.T) {
+	if !assertWrap("hello there", 5, "hello\nthere") {
+		t.Error("failed!")
+	}
+}
+
+func Test_Wrap3(t *testing.T) {
+	if !assertWrap("hello there mom", 5, "hello\nthere\nmom") {
+		t.Error("failed!")
+	}
+}
+
+func Test_Wrap4(t *testing.T) {
+	println("Four score and seven years ago our fathers brought\nforth on this continent, a new nation, conceived in\nLiberty, and dedicated to the proposition that all\nmen are created equal")
+	if !assertWrap(
+		"Four score and seven years ago our fathers brought forth on this continent, a new nation, conceived in Liberty, and dedicated to the proposition that all men are created equal",
+		52, "Four score and seven years ago our fathers brought\nforth on this continent, a new nation, conceived in\nLiberty, and dedicated to the proposition that all\nmen are created equal") {
+		t.Error("failed!")
+	}
+}
+
+func assertWrap(s string, i int, s2 string) bool {
+	println(wrap(s, i))
+	return wrap(s, i) == s2
+}
+
+func wrap(word string, w int) string {
+	if len(word) <= w {
+		return word
+	}
+
+	newString := word[:w]
+	maxIndex := strings.LastIndex(newString, " ")
+
+	if maxIndex != -1 {
+		newString = newString[:maxIndex]
+	} else {
+		maxIndex = w
+	}
+
+	result := strings.Trim(newString, " ") + "\n" + wrap(word[maxIndex+1:], w)
+	return result
+}
