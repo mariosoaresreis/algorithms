78fc4413d6ef09f1b335dc8954af68ab632ba41b
Mario Reis <mario_reis_jr@hotmail.com>
2024-06-27 12:25:46 -0300

    signal: count mines

--- com/marioreis/signal/CountMines_test.go
+++ com/marioreis/signal/CountMines_test.go
@@ -1,40 +1,5 @@
 package signal
 
-import (
-	"testing"
-)
-
-func Test_ExecCountMines(t *testing.T) {
-	matrix := [][]bool{
-		{false, false, false},
-		{false, false, false}}
-	ExecCountMines(matrix)
-
-	//println(len(matrix))
-	//println(len(matrix[0]))
-	/*
-		matrix := [][]int{
-			{0, 1, 2},
-			{3, 4, 5},
-		}
-
-		result := make([][]int, 0)
-		for j := 0; j < len(matrix); j++ {
-			result = append(result, make([]int, len(matrix[j])))
-		}
-
-		for i := 0; i < len(result); i++ {
-			for j := 0; j < len(result[0]); j++ {
-				println(fmt.Sprintf("%d,%d", i, j))
-				println(matrix[i][j])
-			}
-		}
-	*/
-	//println(fmt.Sprintf("%v", matrix))
-	//println(fmt.Sprintf("%v", result))*/
-
-}
-
 func ExecCountMines(matrix [][]bool) [][]int {
 	result := make([][]int, 0)
 
@@ -44,8 +9,9 @@ func ExecCountMines(matrix [][]bool) [][]int {
 
 	maxI := len(result) - 1
 	maxJ := len(result[0]) - 1
-	for i := 0; i < maxI; i++ {
-		for j := 0; j < maxJ; j++ {
+
+	for i := 0; i <= maxI; i++ {
+		for j := 0; j <= maxJ; j++ {
 			result[i][j] = countMines(matrix, i, j, maxI, maxJ)
 
 		}
@@ -72,7 +38,7 @@ func countMines(matrix [][]bool, i int, j int, maxI int, maxJ int) int {
 
 	println("C")
 
-	if (i < len(matrix[0])-1) && (matrix[i+1][j] == true) {
+	if i < maxI && (matrix[i+1][j] == true) {
 		count++
 	}
 	println("D")
