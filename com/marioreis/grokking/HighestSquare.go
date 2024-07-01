6472d0d723274d4d15952931c29d719802a4ce8b
Mario Reis <mario_reis_jr@hotmail.com>
2024-07-01 11:09:35 -0300

    grokking: highest square

---
+++ com/marioreis/grokking/HighestSquare.go
@@ -0,0 +1,27 @@
+package grokking
+
+func MakeSquares(arr []int) []int {
+	n := len(arr)
+	squares := make([]int, n)
+	i := 0
+	j := len(arr) - 1
+	highestIdx := len(arr) - 1
+
+	for i <= j {
+		leftSquare := arr[i] * arr[i]
+		rightSquare := arr[j] * arr[j]
+
+		if leftSquare >= rightSquare {
+			squares[highestIdx] = leftSquare
+			i++
+		}
+
+		if leftSquare < rightSquare {
+			squares[highestIdx] = rightSquare
+			j--
+		}
+		highestIdx--
+	}
+
+	return squares
+}
