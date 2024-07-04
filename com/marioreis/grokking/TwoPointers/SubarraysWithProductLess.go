6d9cbf513e88b4e3365b245e8f44d8d073ee06ce
Mario Reis <mario_reis_jr@hotmail.com>
2024-07-04 10:22:38 -0300

    grokking: sub arrays with product less

---
+++ com/marioreis/grokking/TwoPointers/SubarraysWithProductLess.go
@@ -0,0 +1,31 @@
+package TwoPointers
+
+import "fmt"
+
+func FindSubArrays(arr []int, target int) [][]int {
+	result := make([][]int, 0)
+
+	for left := range arr {
+
+		for k := range arr {
+			product := 1
+			subarray := make([]int, 0)
+
+			for right := left; right <= k; right++ {
+				product = product * arr[right]
+				subarray = append(subarray, arr[right])
+				println(fmt.Sprintf("left=%d", left))
+				println(fmt.Sprintf("right=%d.%v", right, arr[right]))
+				println(fmt.Sprintf("product=%d", product))
+				println(fmt.Sprintf("subarray=%v", subarray))
+			}
+
+			if product < target && len(subarray) > 0 {
+				result = append(result, subarray)
+			}
+		}
+
+	}
+
+	return result
+}
