c05cb76a4adc46a3eaa121045969ecbdc3fe6626
Mario Reis <mario_reis_jr@hotmail.com>
2024-07-17 13:24:53 -0300

    grokking: longest substring

---
+++ com/marioreis/grokking/SlidingWindow/FindLongestSubstring.go
@@ -0,0 +1,28 @@
+package SlidingWindow
+
+func findLength(str string, k int) int {
+	startWindow := 0
+	characterMap := make(map[rune]int)
+	maxCharactersDiff := k
+	maxLength := 0
+
+	for endWindow, rightCharacter := range str {
+		characterMap[rightCharacter]++
+
+		if len(characterMap) > 1 && maxCharactersDiff > 0 {
+			maxCharactersDiff--
+		}
+
+		if maxCharactersDiff == 0 {
+			startWindow++
+			endWindow = startWindow
+			maxCharactersDiff = k
+		}
+
+		if (endWindow - startWindow + 1) > maxLength {
+			maxLength = (endWindow - startWindow + 1)
+		}
+	}
+
+	return maxLength
+}
