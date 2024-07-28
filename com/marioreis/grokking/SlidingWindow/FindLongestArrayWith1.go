f1417dbd8945f1f13301b387f056f4739f86afe1
Mario Reis <mario_reis_jr@hotmail.com>
2024-07-28 20:05:16 -0300

    grokking: Sliding Window - finding highest array with one replacement

---
+++ com/marioreis/grokking/SlidingWindow/FindLongestArrayWith1.go
@@ -0,0 +1,30 @@
+package SlidingWindow
+
+func findLength(arr []int, k int) int {
+	maxLength, windowStart, maxOnesCount := 0, 0, 0
+
+	for windowEnd := 0; windowEnd < len(arr); windowEnd++ {
+		if arr[windowEnd] == 1 {
+			maxOnesCount++
+		}
+
+		if windowEnd-windowStart+1-maxOnesCount > k {
+			if arr[windowStart] == 1 {
+				maxOnesCount--
+			}
+			windowStart++
+		}
+
+		maxLength = max(maxLength, windowEnd-windowStart+1)
+	}
+
+	return maxLength
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+
+	return b
+}
