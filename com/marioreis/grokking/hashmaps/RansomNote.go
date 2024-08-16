9b46bf8e618b427ab78e25e899460a8100127444
Mario Reis <mario_reis_jr@hotmail.com>
2024-08-16 09:47:31 -0300

    improve ransom note

--- com/marioreis/grokking/hashmaps/RansomNote.go
+++ com/marioreis/grokking/hashmaps/RansomNote.go
@@ -2,18 +2,15 @@ package hashmaps
 
 func CanConstruct(ransomNote string, magazine string) bool {
 	mapMagazine := make(map[rune]int)
-	mapRansomNote := make(map[rune]int)
 
 	for _, c := range magazine {
 		mapMagazine[rune(c)]++
 	}
 
-	for _, c := range ransomNote {
-		mapRansomNote[rune(c)]++
-	}
+	for _, c := range mapMagazine {
+		mapMagazine[rune(c)]--
 
-	for k, _ := range mapRansomNote {
-		if mapRansomNote[k] > mapMagazine[k] {
+		if mapMagazine[rune(c)] < 0 {
 			return false
 		}
 	}
