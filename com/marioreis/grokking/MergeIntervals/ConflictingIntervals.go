2120d9cf308e18aa55491af73f9e3c3764fc760a
Mario Reis <mario_reis_jr@hotmail.com>
2024-08-30 10:26:41 -0300

    grokking: conflicting intervals

---
+++ com/marioreis/grokking/MergeIntervals/ConflictingIntervals.go
@@ -0,0 +1,17 @@
+package MergeIntervals
+
+import "sort"
+
+func CanAttendAllAppointments(intervals []Interval) bool {
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i].Start < intervals[j].Start
+	})
+
+	for i := 1; i < len(intervals); i++ {
+		if intervals[i].Start < intervals[i-1].End {
+			return false
+		}
+	}
+
+	return true
+}
