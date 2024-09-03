79693616fe5aa2dc290c32f43af72fcd5a568eba
Mario Reis <mario_reis_jr@hotmail.com>
2024-09-02 21:00:53 -0300

    implementing priority heap

---
+++ com/marioreis/grokking/MergeIntervals/MaxCpuLoad.go
@@ -0,0 +1,40 @@
+package MergeIntervals
+
+import "container/heap"
+
+type Job struct {
+	Start   int
+	End     int
+	CPULoad int
+}
+
+type PriorityQueue []*Job
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+}
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	return pq[i].End < pq[j].End
+}
+
+func (pq PriorityQueue) Len() int {
+	return len(pq)
+}
+
+func (pq PriorityQueue) Push(x interface{}) {
+	pq = append(pq, x.(*Job))
+}
+
+func (pq PriorityQueue) Pop() interface{} {
+	old := pq
+	n := len(old)
+	x := old[n-1]
+	pq = old[0 : n-1]
+	return x
+}
+
+func test() {
+	pq := PriorityQueue{}
+	heap.Init(pq)
+}
