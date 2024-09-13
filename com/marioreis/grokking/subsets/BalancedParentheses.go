4476dc21578ac197f4df372469bde0280f8d4f73
Mario Reis <mario_reis_jr@hotmail.com>
2024-09-13 11:01:18 -0300

    ¨missing parentheses

--- com/marioreis/grokking/subsets/BalancedParentheses.go
+++ com/marioreis/grokking/subsets/BalancedParentheses.go
@@ -1,6 +1,5 @@
 package subsets
 
-
 func GenerateValidParentheses(num int) []string {
 	result := []string{}
 	parenthesesString := make([]rune, num*2)
@@ -9,17 +8,18 @@ func GenerateValidParentheses(num int) []string {
 }
 
 func GenerateParentheses(num int, open int, close int, index int, parenthesesString []rune,
-	result *[]string){
+	result *[]string) {
 	if open == num && close == num {
 		*result = append(*result, string(parenthesesString))
-	}else {
+	} else {
 		if open < num {
 			parenthesesString[index] = '('
-			GenerateParentheses(num, open + 1, close, index+1, parenthesesString, result)
+			GenerateParentheses(num, open+1, close, index+1, parenthesesString, result)
 		}
 
 		if close < open {
 			parenthesesString[index] = ')'
-			GenerateParentheses(num, open, close + 1, index+1, parenthesesString, result)
+			GenerateParentheses(num, open, close+1, index+1, parenthesesString, result)
 		}
 	}
+}
